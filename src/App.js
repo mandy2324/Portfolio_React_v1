4aab7ee9159fc0924bbdbfbbd07a01af9359f934
unknown <jasdeepsaini30@gmail.com>
2021-12-31 14:52:33 -0500

    adding project info in a json file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import About from "./components/About";
 // import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 // import Skills from "./components/Skills";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,9 +12,9 @@ export default function App() {
 <main className="text-gray-200 bg-gray-500 body-font">
       <Navbar />
        <About />
-      {/* <Projects />
-      <Skills />
-      <Contact />  */}
+       <Projects />
+      {/* <Skills /> 
+       <Contact />   */}
     </main>
   );
   
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+import projects from '../../projects.json'
+
+function ProjectCard(props){
+
+    return(
+
+    );
+};
