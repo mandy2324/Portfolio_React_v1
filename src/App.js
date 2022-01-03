428d5712e04f768cd722ff7d1d6c1fa9fb68c52b
unknown <jasdeepsaini30@gmail.com>
2022-01-02 23:37:25 -0500

    adding details to aboutjs

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
     <>
     <div>
       <Header></Header>
+
     </div>
  
     </>
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from 'react'; 
 import myImage from '../assets/images/myimage.png';
-// import flowerImage from '../../assets/images/myflower.png';s
-import { Col } from 'react-bootstrap';
+// import { Col } from 'react-bootstrap';
+
+import { Row, Col } from 'react-bootstrap'; 
+
+import Resume from '../assets/resume.pdf'
+
+
 function About() {
     return(
         <section className="title">
@@ -24,8 +29,44 @@ function About() {
       </Col>
 
       </div>
-    </section> 
+   
+   <div>
+
+
+{/* <h1 data-testid='h1tag' className="contact">Contact Me:</h1> */}
+
+<div className="contact-icons">
+<Row>
+    <Col lg={3} md={12}>
+    <a href="https://github.com/mandy2324"><img src="https://img.icons8.com/doodle/100/000000/github--v1.png" alt="github-logo" class="icon"/></a><p class="icon-text">github</p>
+    </Col>
+
+    <Col lg={3} md={12}>
+    <a href="https://www.linkedin.com/in/m23saini/"><img src="https://img.icons8.com/doodle/100/000000/linkedin--v2.png" alt="linkedin-logo" class="icon"/></a><p class="icon-text">linkedin</p>
+    </Col>
+
+    <Col lg={3} md={12}>
+    <a href="mailto:m23saini@gmail.com"><img src="https://img.icons8.com/doodle/120/000000/new-post.png" alt="mail-logo"/></a><p class="icon-text">email</p>
+    </Col>
+
+    <Col lg={3} md={12}>
+    <a href={Resume} download><img src="https://img.icons8.com/doodle/120/000000/pdf-2.png" alt="resume" /></a><p class="icon-text">resume</p>
+    </Col>
+</Row>
+</div>
+
+</div>
+
+</section>
+
 )}
+
+
+
+
+
+    
+    
 
 export default About;
 
