7d337eef94143de0c6af93cf73c6a489bb3efa4b
unknown <jasdeepsaini30@gmail.com>
2022-01-02 23:49:51 -0500

    adding json file updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 
 import Header from './components/Header';
 
-// import About from "./components/About";
-// import Contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
-// import Skills from "./components/Skills";
+
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
