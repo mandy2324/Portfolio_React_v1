39890bb89aef04fd7df9a16b747f2977184e78b4
unknown <jasdeepsaini30@gmail.com>
2022-01-11 01:00:47 -0500

    adding form js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -29,10 +29,11 @@ export default function ContactForm() {
         
 
 
-        <form
+        <form 
           netlify
           name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="bg-white
+          lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact:
           </h2>
